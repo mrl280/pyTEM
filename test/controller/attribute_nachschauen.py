c02a0a4476661539549429590b061b4f6c738b99
Jason Kobel <jason.kobel@basf.com>
2022-07-14 13:57:05 +0200

    attribute list

--- test/controller/attribute_nachschauen.py
+++ test/controller/attribute_nachschauen.py
@@ -1,14 +1,8 @@
-import os
-import warnings
 import pathlib
 import sys
 
-import numpy as np
-
 
 package_directory = pathlib.Path().resolve()
-print(package_directory)
 sys.path.append(str(package_directory))
 from pyTEM.Interface import Interface
-print(dir(Interface))
-
+print('\n'.join(map(str, dir(Interface))))
